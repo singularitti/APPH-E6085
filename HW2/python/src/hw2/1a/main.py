d1cb1e35c8d75423ef4435138691ea1671011afb
singularitti <singularitti@outlook.com>
2024-06-03 04:14:05 -0400

    Separate `eigenvalue_problem` into `construct_problem` & `solve_problem` in HW2/python/src/hw2/1a/main.py

--- HW2/python/src/hw2/1a/main.py
+++ HW2/python/src/hw2/1a/main.py
@@ -68,10 +68,14 @@ def overlap(a, b):
     return quad(overlap_integrand, 0.0, np.inf, args=(a, b))[0]
 
 
-def eigenvalue_problem(alpha):
-    h_ij = np.asfarray([hamiltonian(a, alpha) for a in alpha])  # Speed-up
+def construct_problem(alphas):
+    h_ij = np.asfarray([hamiltonian(a, alphas) for a in alphas])  # Speed-up
     # Vectorize the table-like function
-    s_ij = np.asfarray([overlap(a, alpha) for a in alpha])
+    s_ij = np.asfarray([overlap(a, alphas) for a in alphas])
+    return h_ij, s_ij
+
+
+def solve_problem(h_ij, s_ij):
     # Generalized eigenvalue Problem H x = E S x
     eigvals, eigvecs = eigh(h_ij, s_ij, eigvals_only=False)
     return eigvals, eigvecs
@@ -82,13 +86,10 @@ if __name__ == "__main__":
     alpha_2 = np.asfarray([13, 1.96])
     alpha_3 = np.asfarray([13, 1.96, 0.44])
     alpha_4 = np.asfarray([13, 1.96, 0.44, 0.12])
-    nbasis = list(map(len, [alpha_1, alpha_2, alpha_3, alpha_4]))
-    energies = [
-        eigenvalue_problem(alpha_1)[0][0],
-        eigenvalue_problem(alpha_2)[0][0],
-        eigenvalue_problem(alpha_3)[0][0],
-        eigenvalue_problem(alpha_4)[0][0],
-    ]
+    alphas = [alpha_1, alpha_2, alpha_3, alpha_4]
+    nbasis = list(map(len, alphas))
+    problems = [construct_problem(alpha) for alpha in alphas]
+    energies = [solve_problem(*problem)[0][0] for problem in problems]
 
     plt.figure()
     plt.plot(nbasis, energies)
@@ -96,6 +97,6 @@ if __name__ == "__main__":
     plt.xlabel(r"$n$", fontsize=16)
     plt.ylabel(r"$E$ (Hartree)", fontsize=16)
     plt.show()
-    print((eigenvalue_problem(alpha_4)[1][:, 0]))
+    print((construct_problem(alpha_4)[1][:, 0]))
     # my_path = os.path.abspath(__file__ + "/../../")
     # plt.savefig(my_path + "/images/pro_1_a.pdf")
