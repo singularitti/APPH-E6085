5b81e1876ca238d7d0d6b54977eb7eac249a1d91
singularitti <singularitti@outlook.com>
2024-06-04 01:17:32 -0400

    Add HW2/python/scripts/1_b.py

---
+++ HW2/python/scripts/1_b.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import matplotlib.style
+import numpy as np
+
+from hw2.ex1 import construct_problem, solve_problem
+
+matplotlib.style.use("classic")
+
+if __name__ == "__main__":
+    alpha_2 = np.asfarray([13, 1.96])
+    alpha_3 = np.asfarray([13, 1.96, 0.44])
+    alpha_4 = np.asfarray([13, 1.96, 0.44, 0.12])
+    alphas = [alpha_2, alpha_3, alpha_4]
+    nbasis = list(map(len, alphas))
+    problems = [construct_problem(alpha) for alpha in alphas]
+    energies = [
+        solve_problem(*problem)[0][1] for problem in problems
+    ]  # First excited states
+    print(energies)
+
+    plt.figure()
+    plt.plot(nbasis, energies)
+    plt.xticks(nbasis)
+    plt.xlabel(r"$n$", fontsize=16)
+    plt.ylabel(r"$E$ (Hartree)", fontsize=16)
+    plt.title("First excited state energies as a function of the number of basis functions")
+    plt.savefig("1_b.pdf")
